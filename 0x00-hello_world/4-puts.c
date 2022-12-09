80e0657138cb31e06a5cca9caeacfa02487be5d3
kevmongare <117772705+kevmongare@users.noreply.github.com>
2022-12-09 05:04:44 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 #include <string.h>
 /**
 * main - entry point
-* 
+*
 *Return: 0 after printing the function
 */
-int main(void) 
+int main(void)
 {
 char str1[] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
