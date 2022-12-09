3974550969f5ff09f74e9b19195ad8d17f307ef1
kevmongare <117772705+kevmongare@users.noreply.github.com>
2022-12-09 04:39:27 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <string.h>
+/**
+* main - entry point
+* 
+*Return: 0 after printing the function
+*/
 int main(void)
 {
-char str1[] ="Programming is like building a multilingual puzzle";
-puts(str1);
-return 0;
-}
+Puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}	
