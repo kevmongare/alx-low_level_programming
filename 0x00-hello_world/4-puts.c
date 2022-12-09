fbd29c9e98464ee3994d7c5f624c3c4bb38667be
kevmongare <117772705+kevmongare@users.noreply.github.com>
2022-12-09 04:35:29 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main()
 {
-char str1[] ="Programming is like building a multilingual puzzle/n";
-puts (str1);
+char str1[] ="Programming is like building a multilingual puzzle";
+puts (str1 /n);
 return 0;
 }
